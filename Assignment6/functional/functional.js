5ed672afa3a71f867534a31500369fea49336482
Dvali <108537326+NikolozDvali@users.noreply.github.com>
2023-04-13 18:20:52 +0400

    organized js into separate files, added location sharing option

--- Assignment6/functional/functional.js
+++ Assignment6/functional/functional.js
@@ -1,3 +1,6 @@
+import { getWeatherData } from "./weather.js";
+import { Popup } from "./popup.js";
+import { List } from "./list.js";
 (function () {
     let state = undefined;
 
@@ -18,147 +21,6 @@
         return [state, setValue];
     }
 
-    //returns list title;
-    function ListTitle(title){
-        const h1 = document.createElement('h1');
-        h1.innerText = title;
-        h1.classList.add("listTitle");
-        return h1;
-    }
-
-    //returns radio element;
-    function RadioElement(items, item, index) {
-        const { isCompleted } = item;
-        
-        const updateItems = (isCompleted) => {
-          items[index].isCompleted = isCompleted;
-          const updatedTask = items[index];
-            fetch(`http://localhost:3004/tasks/${item.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(updatedTask)
-                })
-                .catch(error => console.error(error));
-        };
-        
-        const radioElement = document.createElement("input");
-        radioElement.type = "checkbox";
-        radioElement.checked = isCompleted;
-        radioElement.onchange = (event) => {
-          const isCompleted = event.target.checked;
-          updateItems(isCompleted);
-        };
-        
-        return radioElement;
-      }
-
-    //Image is reserved word so I called this function 'MyImage' :)
-    //returns remove image for uncompleted tasks;
-    function MyImage(item, setItems) {
-
-        const { src, classList } = {
-          src: 'images/Delete.svg',
-          classList: ['deleteIcon'],
-        };
-      
-        const deleteItem = () => {
-          fetch(`http://localhost:3004/tasks/${item.id}`, {
-            method: 'DELETE'
-            }).catch(error => console.error(error));
-        };
-      
-        const img = document.createElement('img');
-        img.src = src;
-        img.classList.add(...classList);
-        img.onclick = deleteItem;
-        
-        return img;
-      }
-      
-    function Tag(title, displayCompleted, cursorOn){
-        const tag = document.createElement("div");
-        const className = `listItem__Tag--${title}--${displayCompleted ? 'isCompleted' : 'uncompleted'}`;
-        tag.classList.add(className);
-        tag.innerText = title;
-        if(cursorOn){
-            tag.classList.add("cursorOnTag");
-        }
-        return tag;
-    }
-
-    function MiddleOfListItem(displayCompleted, item){
-        const mid = document.createElement("div");
-                mid.classList.add("listItem__Middle")
-                const pElement = document.createElement('p');
-                pElement.classList.add(`taskParagraph--${displayCompleted ? 'isCompleted' : 'uncompleted'}`);
-                pElement.textContent = item.title;
-
-                const attributes = document.createElement("div");
-                attributes.classList.add("listItem__Attributes");
-
-                const tag = Tag(item.tag, displayCompleted);
-
-                const date = document.createElement("p");
-                date.classList.add("listItem__date");
-                date.innerText = item.date;
-
-                attributes.appendChild(tag);
-                attributes.appendChild(date);
-        
-                mid.appendChild(pElement);
-                mid.appendChild(attributes);
-        return mid;
-    }
-
-    //returns list itself, 'displayCompleted' is bool that indicates whether it should be list of isCompleted tasks or uncompleted tasks;
-    function ListCompletedAndUncompleted(items, setItems, displayCompleted) {
-        const div = document.createElement('div');
-        div.classList.add(`listsDiv--${displayCompleted ? 'isCompleted' : 'uncompleted'}`);
-        div.appendChild(ListTitle(displayCompleted ? 'isCompleted Tasks' : 'All tasks'));
-        const ul = document.createElement('ul');
-        ul.classList.add('ul');
-        items.forEach((item, index) => {
-            if (item.isCompleted === displayCompleted) {
-                const li = document.createElement('li');
-                li.classList.add(`listItem--${displayCompleted ? 'isCompleted' : 'uncompleted'}`);
-                li.setAttribute("id", `listItem--${displayCompleted ? 'isCompleted' : 'uncompleted'}--${index}`)
-    
-                const radioElement = RadioElement(items, item, index, setItems);
-                li.appendChild(radioElement);
-    
-                const mid = MiddleOfListItem(displayCompleted, item);
-                li.appendChild(mid);
-    
-                if (!displayCompleted) {
-                    const img = MyImage(item, setItems);
-                    li.appendChild(img);
-                }
-    
-                ul.appendChild(li);
-            }
-        });
-        div.appendChild(ul);
-        return div;
-    }
-
-    /**
-     * Functional component for the list
-     * @param items {string[]}
-     * @returns {HTMLElement} - List element
-     */
-    function List(items, setItems) {
-        const div = document.createElement("div");
-        
-        const uncompletedList = ListCompletedAndUncompleted(items, setItems, false);
-        const completedList = ListCompletedAndUncompleted(items, setItems, true);
-        
-        div.classList.add("lists");
-        div.appendChild(uncompletedList);
-        div.appendChild(completedList);
-        return div;
-      }
     /**
      * Button component
      * @param text {string}
@@ -247,143 +109,6 @@
         return header;
     }
 
-    //returns popup title paragraph;
-    function PopupTitle(title){
-        const p = document.createElement('p');
-        p.innerText = title;
-        p.classList.add("popupTitle")
-        return p;
-    }
-
-    //returns text input for pupup;
-    function PopupField(){
-        const field = document.createElement("input");
-        field.classList.add("popupField");
-        field.placeholder = "Task Title";
-        field.type = "text";
-        return field;
-    }
-
-    //returns array of objects (tags);
-    // {html: tag itself, title: title of the tag}
-    function TagInputs(){
-        const healthTag = {html: Tag("health", false, true), title: "health"};
-        const workTag = {html: Tag("work", false, true), title: "work"};
-        const homeTag = {html: Tag("home", false, true), title: "home"};
-        const otherTag = {html: Tag("other", false, true), title: "other"};
-        return [healthTag, workTag, homeTag, otherTag];
-    }
-
-    //removes --selected class from all tags;
-    function makeAllTagsUnselected(tags){
-        for(let i = 0; i<tags.length; i++){
-            tags[i].html.classList.remove(`tagsInput__${tags[i].title}--selected`);
-        }
-    }
-
-    //when tag is clicked, it becomes selected tag;
-    //other tags lose class --selected;
-    function addEventListenerToTag(selectedTag, tagInputs, i){
-        tagInputs[i].html.addEventListener("click", ()=>{
-            selectedTag.tag = tagInputs[i].title;
-            makeAllTagsUnselected(tagInputs);
-            tagInputs[i].html.classList.add(`tagsInput__${selectedTag.tag}--selected`);
-        })
-    }
-
-    //returns tags input div;
-    function TagsInput(selectedTag){
-        const div = document.createElement("div");
-        div.classList.add("tagsInput");
-        const tagInputs = TagInputs();
-        for(let i = 0; i<tagInputs.length; i++){
-            addEventListenerToTag(selectedTag, tagInputs, i);
-            div.appendChild(tagInputs[i].html);
-        }
-        return div;
-    }
-
-    //returns date input;
-    function DateInput(){
-        const date = document.createElement("input");
-        date.type = "date";
-        date.classList.add("dateInput");
-        date.onchange = ()=>{
-            if(date.value!=''){
-                date.classList.add("dateInput--filled");
-            }else{
-                date.classList.remove("dateInput--filled");
-            }
-        }
-        return date;
-    }
-
-    /*  Returns popup for adding new item into list;
-    */
-    function Popup(){
-        let buttonIsValid = false;
-        //keeps track of which tag is selected;
-        //is object because needs to be passed as reference;
-        let selectedTag = {tag: null};
-        //function to add new item into list;
-        //new item is added when all inputs are placed;
-        function addItem(){
-            if(buttonIsValid){
-                function toValidForm(date){
-                    const dt = new Date(date);
-                    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-                    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                    const output = days[dt.getDay()] + ", " + dt.getDate() + " " + months[dt.getMonth()];
-                    return output;
-                }
-                fetch('http://localhost:3004/tasks', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ title: document.getElementsByClassName("popupField")[0].value, isCompleted: false, tag: selectedTag.tag, date: toValidForm(dateInput.value)})
-                    })
-                    .catch(error => console.error(error));
-                selectedTag.tag = null;
-            }
-        }
-        //function to close popup;
-        //makes selected tag null;
-        function closePopup(){
-            document.getElementsByClassName("popupField")[0].value = "";
-            document.getElementsByClassName("popupDiv")[0].style.display="none";
-            selectedTag.tag=null;
-        }
-        const cancelButton = Button({text: "Cancel", onClick: closePopup, class: "cancelButton"});
-        //checks if all inputs are valid and changes button display;
-        //changes buttonIsValid boolean;
-        function validateButton(){
-            if(selectedTag.tag!=null && dateInput.value!='' && field.value.length>0){
-                addButton.classList.add("addTaskButton--blue");
-                buttonIsValid = true;
-            }else{
-                addButton.classList.remove("addTaskButton--blue");
-                buttonIsValid = false;
-            }
-        }
-
-        const div = document.createElement("div");
-        const popupTitle = PopupTitle("Add New Task");
-        const addButton = Button({text: "Add Task", onClick: addItem ,class: "addTaskButton--grey"});
-        const field = PopupField(selectedTag);
-        const tagsInput = TagsInput(selectedTag);
-        const dateInput = DateInput();
-
-        const fields = [field, tagsInput, dateInput];
-        fields.forEach((input) => {
-            input.oninput = validateButton;
-        });
-
-        div.classList.add("popupDiv");
-        div.append(popupTitle, field, tagsInput, dateInput, addButton, cancelButton);
-        return div;
-    }
-
     /**
      * App container
      * @returns {HTMLDivElement} - The app container
@@ -418,24 +143,6 @@
         return div;
       }
 
-
-      function getWeatherData(){
-        function setIcon(data){
-            let iconUrl = data.condition.icon;
-            iconUrl = iconUrl.substring(20, iconUrl.length);
-            document.getElementsByClassName("weather__icon")[0].src = `/Assignment6/images${iconUrl}`
-        }
-
-        const key = "95da8a101a2b4fdb8bb185943231204";
-        const city = "Tbilisi";
-        const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
-        fetch(url).then(response=>response.json()).then(data => {
-            document.getElementsByClassName("weather__degrees")[0].innerHTML=`${data.current.temp_c}&#176`;
-            setIcon(data.current);
-        }
-            ).catch(error=>console.log(error));
-    }
-
     /**
      * Render the app.
      * On change whole app is re-rendered.
@@ -444,11 +151,9 @@
         const appContainer = document.getElementById("functional-example");
         appContainer.innerHTML = "";
         appContainer.append(App());
-        //get weather data;
         getWeatherData();
     }
 
     // initial render
     renderApp();
-
 })();
---
+++ Assignment6/functional/weather.js
@@ -0,0 +1,50 @@
+export function getWeatherData() {
+    // Function to set the weather ic
+    function setWeatherIcon(data) {
+      let iconUrl = data.condition.icon;
+      iconUrl = iconUrl.substring(20, iconUrl.length);
+      document.getElementsByClassName("weather__icon")[0].src = `/Assignment6/images${iconUrl}`;
+    }
+  
+    // Weather API key and default city
+    const API_KEY = "95da8a101a2b4fdb8bb185943231204";
+    let city = "Tbilisi";
+
+    const getDataFromWeatherAPI = ()=>{
+         // Get weather data from Weather API
+         const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+         fetch(url)
+           .then(response => response.json())
+           .then(data => {
+             // Update weather information on the page
+             document.getElementsByClassName("weather__degrees")[0].innerHTML = `${data.current.temp_c}&#176`;
+             document.getElementsByClassName("weather__location")[0].innerHTML = data.location.name;
+             setWeatherIcon(data.current);
+           })
+           .catch(error => console.log(error));
+    }
+    getDataFromWeatherAPI();
+
+  
+    // Callback functions for geolocation API
+    const successCallback = (position) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => {
+          city = data.localityInfo.administrative[2].name;
+            getDataFromWeatherAPI();
+        });
+    }
+  
+    const errorCallback = () => {
+      // If geolocation fails, use default city
+      city = "Tbilisi";
+      getDataFromWeatherAPI();
+    }
+  
+    // Get the user's location using geolocation API
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+  
